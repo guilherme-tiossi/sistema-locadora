c00cb83938bc7ae286cc17704d8a65b9d0debee5
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-07-12 18:54:23 -0300

    alteração no sistema de verificação de login

--- cadastro.php
+++ cadastro.php
@@ -1,9 +1,34 @@
 <?php
+session_start();
 include("conexao.php");
-include("verificacao.php");
-if(isset($email)){
-    header("Location: home.php");
-};
+
+if(isset($_SESSION['email'])){
+$email = $_SESSION['email'];
+$senha = $_SESSION['senha'];
+$stmt = $pdo->prepare("select * from tbusuarios where email_user = '$email' and senha_user = '$senha'");$stmt->execute();
+$count = $stmt->rowCount();
+
+foreach ($stmt as $row){
+    $adm = $row['adm'];
+    $id = $row['id_user'];
+}
+
+if ($count == 1 && $adm == 1){
+    $_SESSION['email'] = $email;
+    $_SESSION['senha'] = $senha;
+    $_SESSION['id_user'] = $id;
+    header("Location: home_adm.php");
+    exit();
+}
+
+else if ($count ==1 && $adm == 0){
+    $_SESSION['email_user'] = $email;
+    $_SESSION['senha_user'] = $senha;
+    $_SESSION['id_user'] = $id;
+    header("Location: home_user.php");
+    exit();
+}
+}
 ?>
 
 <html>
@@ -13,5 +38,6 @@ if(isset($email)){
     <input type="password" name="senha">
     <input type="submit"> 
     </form>
+    <a href="login.php"> Login </a>
 
 </html>
--- exec_login.php
+++ exec_login.php
@@ -6,12 +6,23 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
     $stmt = $pdo->prepare("select email_user from tbusuarios where email_user = '$email' and senha_user = '$senha'");
     $stmt->execute();
-    $row = $stmt->rowcount();
-    $stmt->fetch();
+    $count = $stmt->rowcount();
 
-if ($row == 1){
+foreach ($stmt as $row){
+    $adm = $row['adm'];
+}
+
+if ($count == 1 && $adm == 1){
     $_SESSION['email'] = $email;
-    header("Location: home.php");
+    $_SESSION['senha'] = $senha;
+    header("Location: home_adm.php");
+    exit();
+}
+
+else if ($count ==1 && $adm == 0){
+    $_SESSION['email'] = $email;
+    $_SESSION['senha'] = $senha;
+    header("Location: home_user.php");
     exit();
 }
 else{
