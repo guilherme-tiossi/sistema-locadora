b7069af5df459836fb9478e99eedf9c9728f0b55
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-07-13 01:55:50 -0300

    alteração na exibição da categoria usando agora o inner join

--- exec_filme.php
+++ exec_filme.php
@@ -4,6 +4,12 @@ include ("verificacao.php");
 
 $filme = $_POST['tx_filme'];
 $genero = $_POST['slct_genero'];
+$stmt = $pdo->prepare("select * from tbgeneros where genero = '$genero'");
+$stmt->execute();
+foreach ($stmt as $row){
+    $genero = $row['id_genero'];
+}
+
 $id = $_POST['hd_id'];
 if ($_POST["hd_id"] > 0){
 $stmt = $pdo->prepare("update tbfilmes set titulo_filme='$filme', idgenero='$genero' where id_filme='$id'");
@@ -13,5 +19,5 @@ else{
 $stmt = $pdo->prepare("insert into tbfilmes values ('', '$filme', '$genero')");
 $stmt->execute();
 }
-header("Location: home.php");
-?>
+header("Location: home_adm.php");
+?> 
